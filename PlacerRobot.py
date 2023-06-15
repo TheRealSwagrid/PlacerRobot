18eab45d304fbcca44cac98981969945eadb0a5a
rosrunner <luca.alfano@uni-a.de>
2023-06-15 16:20:27 +0200

    adding some sleep

--- PlacerRobot.py
+++ PlacerRobot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import signal
 import sys
+from time import sleep
 
 from AbstractVirtualCapability import AbstractVirtualCapability, VirtualCapabilityServer, formatPrint
 
@@ -33,8 +34,9 @@ class PlacerRobot(AbstractVirtualCapability):
             self.position[1] += current_vel
             if self.funtionality["set_pos_viz"] is not None:
                 self.funtionality["set_pos_viz"](self.position)
+            sleep(current_vel*1.5)
 
-        return {"Position3D":self.position}
+        return {"Position3D": self.position}
 
     def GetPosition(self, params: dict):
         formatPrint(self, f"Get Position {params}")
