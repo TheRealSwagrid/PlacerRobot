8ad7c251983ada083b748f4e337473cc547ef310
Luca Alfano <luca.alfano@t-online.de>
2023-06-28 15:06:20 +0200

    kk

--- PlacerRobot.py
+++ PlacerRobot.py
@@ -35,7 +35,7 @@ class PlacerRobot(AbstractVirtualCapability):
             self.position[1] += current_vel
             if self.funtionality["set_pos_viz"] is not None:
                 self.funtionality["set_pos_viz"](self.position)
-            sleep(abs(current_vel*2))
+            sleep(1 + abs(current_vel*2))
 
         return {"Position3D": self.position}
 
