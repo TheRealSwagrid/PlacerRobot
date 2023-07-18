fixed rotation

ros_ws/src/robothandler/src/robot_handler.py
@@ -85,8 +85,8 @@ class RobotHandler:
 
         pos = copy(self.position)
 
-        pos[2] += .2
-        pos[1] += .3
+        pos[2] += .05
+        pos[1] += .2
 
         rot = list(quaternion_about_axis(np.deg2rad(90.), [0,0,1]))
         self.br.sendTransform(pos.tolist(),