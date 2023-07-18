fixed rotation

ros_ws/src/robothandler/src/robot_handler.py
@@ -2,6 +2,7 @@
 
 import rospy
 import tf
+import numpy as np
 from tf.transformations import euler_from_quaternion, quaternion_about_axis
 from AbstractVirtualCapability import VirtualCapabilityServer
 from visualization_msgs.msg import Marker
@@ -56,7 +57,8 @@ class RobotHandler:
         marker.mesh_resource = r"package://robothandler/meshes/robot.dae"
         self.pub.publish(marker)
 
-        rot = list(quaternion_about_axis(90, [0,0,1]))
+
+        rot = list(quaternion_about_axis(np.deg2rad(90.), [0,0,1]))
         self.br.sendTransform(self.position,
                               rot, rospy.Time.now(), self.name, "world")
 