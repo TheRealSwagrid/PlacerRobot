fixed rotation

ros_ws/src/robothandler/src/robot_handler.py
@@ -58,7 +58,8 @@ class RobotHandler:
         self.pub.publish(marker)
 
         pos = copy(self.position)
-        pos[2] += .2
+        pos[2] += .05
+        pos[0] += .1
         rot = list(quaternion_about_axis(np.deg2rad(90.), [0,0,1]))
         self.br.sendTransform(pos,
                               rot, rospy.Time.now(), self.name, "world")