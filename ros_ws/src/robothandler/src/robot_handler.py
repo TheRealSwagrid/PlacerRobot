a3ad2fa45c9972af1ae25244048973471ef4769f
rosrunner <luca.alfano@uni-a.de>
2023-11-07 17:36:26 +0100

    norming

--- ros_ws/src/robothandler/src/robot_handler.py
+++ ros_ws/src/robothandler/src/robot_handler.py
@@ -90,7 +90,8 @@ class RobotHandler:
             self.position += current_vel
 
             self.publish_visual()
-            sleep((abs(current_vel[0]) + abs(current_vel[1]) + abs(current_vel[2])))
+            # TODO wait appropriate
+            sleep(np.sum(np.abs(current_vel)) * .1)
             vel += self.acc
             vel = min(vel, self.max_vel)
 
