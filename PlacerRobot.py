effbb7181f11cbe368f1e861402ba36022f8dc03
rosrunner <luca.alfano@uni-a.de>
2024-01-11 13:46:39 +0100

    rotated robot.dae

--- PlacerRobot.py
+++ PlacerRobot.py
@@ -15,7 +15,7 @@ class PlacerRobot(AbstractVirtualCapability):
     def __init__(self, server):
         super().__init__(server)
         self.uri = f"PlacerRobot"
-        self.direction = [0., 1., 0.]
+        self.direction = [1., 0., 0.]
         self.position = [0., 0., 0.]
         # x, y, z, w
         self.rotation = [0., 0., 0., 1.]
